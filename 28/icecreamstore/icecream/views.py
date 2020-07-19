5cc94fcc541e8d39c95f5f6e2faf917457a3f93d
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 18:08:54 +0200

    28

---
+++ 28/icecreamstore/icecream/views.py
@@ -0,0 +1,30 @@
+import random
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+
+from .models import Icecream
+from .serialize import IcecreamSerializer
+
+
+def hello_view(request):
+    return HttpResponse("Hello")
+
+
+def get_random_icecream(request):
+    items = Icecream.objects.all()
+    id = random.randrange(1, Icecream.objects.all().count())
+    icecream = Icecream.objects.get(id=items[id].id)
+
+    return JsonResponse({'icecream': icecream.name})
+
+
+
+class IcecreamView(viewsets.ModelViewSet):
+    serializer_class = IcecreamSerializer
+    queryset = Icecream.objects.all()
+
+

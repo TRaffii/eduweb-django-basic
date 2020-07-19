6579ec629ea845486efa286f2108b6e93052161b
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 18:00:17 +0200

    25-finish

--- 25/icecreamstore/icecream/models.py
+++ 25/icecreamstore/icecream/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Icecream(models.Model):
+    name = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=10000)
+

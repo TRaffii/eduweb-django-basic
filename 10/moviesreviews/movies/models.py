6efd81f68f5809084cefecefe3d430a5e4ebb040
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 16:58:19 +0200

    10-finish

--- 10/moviesreviews/movies/models.py
+++ 10/moviesreviews/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image_url = models.CharField(max_length=1000)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)

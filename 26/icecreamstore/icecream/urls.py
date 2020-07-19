baba57df0f8e4459cee5d7374df50d042801d414
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 18:03:31 +0200

    26-finish

--- 26/icecreamstore/icecream/urls.py
+++ 26/icecreamstore/icecream/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register('icecream', views.IcecreamView, 'icecream')
 urlpatterns = [
     path('', views.hello_view),
-
+    path('api', include(router.urls))
 ]

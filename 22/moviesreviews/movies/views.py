232585bf6e090102d19461cbf74dc13de75a3335
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 17:46:15 +0200

    22

---
+++ 22/moviesreviews/movies/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+# Create your views here.
+from .forms import ReviewForm
+from .models import Movie, Review
+
+
+def show_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'index_movies.html', {'movies': movies})
+
+
+def add_review(request):
+    form = ReviewForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
+    context = {
+        'form': form
+    }
+    return render(request, "add_review.html", context)
+
+
+def show_movie(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    reviews = Review.objects.filter(movie=movie)
+
+    context = {
+        'movie': movie,
+        'reviews': reviews,
+        'error': None
+    }
+
+    return render(request, "show_movie.html", context)

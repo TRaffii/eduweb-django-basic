0c5e6b8e36e0a4b2c98221255bc6e95b8d534ffc
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 16:44:01 +0200

    08-finish

--- 08/coursetestproject/reviews/urls.py
+++ 08/coursetestproject/reviews/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('add', views.add_question),
-    path('question/<int:question_id>/', views.show_question)
+    path('show/<int:question_id>/', views.show_question),
+    re_path(r'^find/(?P<year>[0-9]{4})/$', views.show_by_year)
 ]
--- 08/coursetestproject/reviews/views.py
+++ 08/coursetestproject/reviews/views.py
@@ -6,3 +6,8 @@ from django.http import HttpResponse
 def add_question(request):
     return HttpResponse("Adding a review")
 
+
+def show_question(request, question_id):
+    print(f"Passed question id: {question_id}")
+    return HttpResponse("Showing question...")
+

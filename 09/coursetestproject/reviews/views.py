af66751044a8b708a57c216b25bc421c01fed5b0
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 16:46:46 +0200

    09-finish

--- 09/coursetestproject/reviews/views.py
+++ 09/coursetestproject/reviews/views.py
@@ -11,3 +11,9 @@ def show_question(request, question_id):
     print(f"Passed question id: {question_id}")
     return HttpResponse("Showing question...")
 
+
+def show_by_year(request, year):
+    print(f'Year is: {year}')
+    if year is not None:
+        return HttpResponse(f"Year if {year}")
+    return HttpResponse(f"Year is missing")

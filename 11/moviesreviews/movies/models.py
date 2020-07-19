d8840ccc51825d9365dccb0235a29316c544202e
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 17:01:35 +0200

    11-finish

--- 11/moviesreviews/movies/models.py
+++ 11/moviesreviews/movies/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image_url = models.CharField(max_length=1000)
+    image_url = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Review(models.Model):

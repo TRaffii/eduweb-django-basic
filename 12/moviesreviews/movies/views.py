701c31e28af07a43ec26bd504515b89a44e47fab
Record <record@Rafas-MacBook-Pro.local>
2020-07-19 17:04:54 +0200

    12-finish

--- 12/moviesreviews/movies/views.py
+++ 12/moviesreviews/movies/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Movie
+
+
+def show_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'index_movies.html', {'movies': movies})
